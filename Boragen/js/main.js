8829adda1371735c46eabb576f39143180284f7b
Igor <45938870+igor-cher@users.noreply.github.com>
2020-05-21 14:30:06 +0300

    Update main.js

--- Boragen/js/main.js
+++ Boragen/js/main.js
@@ -28,12 +28,6 @@
     document.querySelectorAll(".inv").forEach( (el)=>{
         el.classList.toggle("invToggle");
     });
-    this.classList.toggle("opened");
-    if(this.classList.contains("opened")){
-        this.innerHTML = "hide investors";
-    } else{
-        this.innerHTML = "more investors";
-    }
   };
   
   button.addEventListener("click", showItems);
