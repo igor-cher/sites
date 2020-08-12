7c353123bc9644ded7b52e70cade7d0490a9f8ef
Igor <45938870+igor-cher@users.noreply.github.com>
2020-08-12 21:37:26 +0300

    Add files via upload

---
+++ landing-school/main.js
@@ -0,0 +1,84 @@
+// header menu
+
+let showMenu = () => {
+    document.querySelector(".btn").classList.toggle('active')
+    document.querySelector(".header").classList.toggle('header-active')
+}
+
+document.querySelector(".menu").addEventListener('click', (e) => {
+    e.preventDefault()
+
+    showMenu()
+})
+
+
+document.querySelectorAll("nav a").forEach( link => {
+    link.addEventListener('click', (e)=>{
+        showMenu()
+    })
+})
+
+
+// idea list
+
+
+let btns = document.querySelectorAll(".idea-button button").forEach(btn => {
+    btn.addEventListener('click', (e)=>{
+        e.preventDefault()
+        e.target.classList.toggle('idea-button-active')
+        e.target.parentNode.parentNode.nextElementSibling.classList.toggle('idea-list-active')
+    })
+})
+
+
+$(document).ready(function (){
+    $(".slider-main").slick({
+        infinite: false,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: document.querySelector(".prev"),
+        nextArrow: document.querySelector(".next"),
+        cssEase: 'linear',
+        
+    });
+
+});
+
+
+let counter = 1;
+let selected = document.getElementById('selected');
+let prev = document.querySelector('.prev');
+let next = document.querySelector('.next')
+selected.innerHTML = counter
+
+ 
+document.addEventListener('click', (e)=> {
+    e.stopPropagation()
+
+    if(e.target.classList.contains("prev")){
+        if(counter === 1 ){
+            prev.disabled = true;
+        } else{
+            prev.disabled = false
+            next.disabled = false
+            counter--;
+            selected.innerText = counter;
+        }
+    
+    } else if(e.target.classList.contains("next")){
+        if(counter === 3){
+            next.disabled = true;
+        } else{
+            next.disabled = false
+            counter++;
+            selected.innerText = counter;
+        }
+    }
+
+    return counter;
+})
+
+
+
+
